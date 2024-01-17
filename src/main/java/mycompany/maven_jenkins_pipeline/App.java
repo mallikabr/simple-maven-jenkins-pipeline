813b2eb0f802df157e8e81a9c10e87f96c0b8638
Mallika-BR <mallika.bagepallirajasekhara@ucdenver.edu>
2024-01-17 00:14:56 -0700

    first commit

---
+++ src/main/java/mycompany/maven_jenkins_pipeline/App.java
@@ -0,0 +1,13 @@
+package mycompany.maven_jenkins_pipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
